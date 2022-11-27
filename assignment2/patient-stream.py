87a375b8e315b3174a2626135a6645260d50ddc8
smoothstill <59178987+smoothstill@users.noreply.github.com>
2022-11-27 18:46:12 +0200

    code improvements, exponential distribution

--- assignment2/patient-stream.py
+++ assignment2/patient-stream.py
@@ -1,78 +1,92 @@
 import numpy as np # version 1.23.3
 import simpy # version 4.0.1
 # Python version 3.10.6
-import argparse
-
-env = simpy.Environment()
-
-def clamp(num, min_value, max_value):
-   return max(min(num, max_value), min_value)
+import random
 
 # Constants of the simulation. Assuming that time is in minutes
-NUM_PREPARATION_FACILITIES = 3
-NUM_OPERATION_FACILITIES = 1
-NUM_RECOVERY_FACILITIES = 3
-AVG_PREPARATION_TIME = 40
-AVG_OPERATION_TIME = 20
-AVG_RECOVERY_TIME = 40
+N_PREPARATION_FACILITIES = 3
+N_OPERATION_FACILITIES = 1
+N_RECOVERY_FACILITIES = 3
+PREPARATION_TIME = 40
+OPERATION_TIME = 20
+RECOVERY_TIME = 40
 PATIENT_INTERARRIVAL_TIME = 24
-SIM_TIME = 60*10
-COMPLICATION_RATIO = 0.01 # If complications happen during the operation,
-# patient will skip recovery and go to ER instead
-DEBUG = True # If true, print logs during the simulation
+SIM_TIME = 1000
+COMPLICATION_CHANGE = 0.01 # If complications happen during the operation, patient will skip recovery and go to ER instead
 RANDOM_SEED = None # Set to none if don't want to use seed
+VERBOSE = 5
 
-np.random.seed(RANDOM_SEED)
+class Logger:
+  verbose = 5
+  def __init__(self, verbose=5):
+    self.verbose = verbose
 
-# Class for managing empty and reserved facilities
+  def set_verbose(self, verbose):
+    self.verbose = verbose
+
+  def log(self, msg, verbose_level = 5):
+    if verbose_level <= self.verbose:
+      print(msg)
+    else:
+      return
+
+logger = Logger(VERBOSE)
+env = simpy.Environment()
+
 class FacilitiesManager:
   """Managing resources of failicities processing their services"""
   preparation_facilities = None
   operation_facilities = None
   recovery_facilities = None
+  prep_time = None
+  op_time = None
+  rec_time = None
+  compl_ratio = None
   env = None
-  debug = False
 
-  def __init__(self, env, debug):
+  def __init__(self, env, p = N_PREPARATION_FACILITIES, o = N_OPERATION_FACILITIES, r = N_RECOVERY_FACILITIES, prep_time = PREPARATION_TIME, op_time = OPERATION_TIME, rec_time = RECOVERY_TIME, compl_chance = COMPLICATION_CHANGE):
     self.env = env
-    self.debug = debug
-    self.preparation_facilities = simpy.Resource(env, NUM_PREPARATION_FACILITIES)
-    self.operation_facilities = simpy.Resource(env, NUM_OPERATION_FACILITIES)
-    self.recovery_facilities = simpy.Resource(env, NUM_RECOVERY_FACILITIES)
+    self.preparation_facilities = simpy.Resource(env, p)
+    self.operation_facilities = simpy.Resource(env, o)
+    self.recovery_facilities = simpy.Resource(env, r)
+    self.prep_time = prep_time
+    self.op_time = op_time
+    self.rec_time = rec_time
+    self.compl_ratio = compl_chance
 
   def preparation(self, patient):
-    if self.debug: print(f"{patient} enters preparation facility at {self.env.now:.2f}")
+    logger.log(f"{patient} enters preparation facility at {self.env.now:.2f}", 5)
     patient.preparation_start_time = float(self.env.now)
-    random_time = clamp(np.random.normal(AVG_PREPARATION_TIME, 4), AVG_PREPARATION_TIME - 5, AVG_PREPARATION_TIME + 5)
+    random_time = random.expovariate(self.prep_time)
     yield self.env.timeout(random_time)
-    if self.debug: print(f"{patient} leaves preparation facility at {self.env.now:.2f}")
+    logger.log(f"{patient} leaves preparation facility at {self.env.now:.2f}", 5)
     patient.preparation_end_time = float(self.env.now)
 
   def operation(self, patient):
-    if self.debug: print(f"{patient} enters operation facility at {self.env.now:.2f}")
+    logger.log(f"{patient} enters operation facility at {self.env.now:.2f}", 5)
     patient.operation_start_time = float(self.env.now)
-    random_time = clamp(np.random.normal(AVG_OPERATION_TIME, 4), AVG_OPERATION_TIME - 5, AVG_OPERATION_TIME + 5)
+    random_time = random.expovariate(self.op_time)
     yield self.env.timeout(random_time)
-    if self.debug: print(f"{patient} leaves operation facility at {self.env.now:.2f}")
+    logger.log(f"{patient} leaves operation facility at {self.env.now:.2f}", 5)
     patient.operation_end_time = float(self.env.now)
 
   def recovery(self, patient):
-    if (np.random.uniform(0, 1) <= COMPLICATION_RATIO):
-      if self.debug: print(f"complication occurred for {patient}, patient skips recovery")
+    if (np.random.uniform(0, 1) <= self.compl_ratio):
+      logger.log(f"complication occurred for {patient}, patient skips recovery", 5)
       patient.complication_occurred = True
       patient.recovery_start_time = float(self.env.now)
       patient.recovery_end_time = float(self.env.now)
       Patient.complications += 1
     else:
-      if self.debug: print(f"{patient} enters recovery facility at {self.env.now:.2f}")
+      logger.log(f"{patient} enters recovery facility at {self.env.now:.2f}", 5)
       patient.recovery_start_time = float(self.env.now)
-      random_time = clamp(np.random.normal(AVG_RECOVERY_TIME, 4), AVG_RECOVERY_TIME - 5, AVG_RECOVERY_TIME + 5)
+      random_time = random.expovariate(self.rec_time)
       yield self.env.timeout(random_time)
-      if self.debug: print(f"{patient} has left recovery facility at {self.env.now:.2f}")
+      logger.log(f"{patient} has left recovery facility at {self.env.now:.2f}", 5)
       patient.recovery_end_time = float(self.env.now)
       patient.complication_occurred = False
 
-# Patient class to store individual patient's information
+
 class Patient:
   """Manages single patient's life cycle. Stores data of individual patient and keeps track of 
   all arrived patients and treated patients"""
@@ -88,14 +102,12 @@ class Patient:
   recovery_end_time = None
   complication_occurred = None
   env = None
-  debug = False
 
-  def __init__(self, env, debug):
+  def __init__(self, env):
     self.env = env
-    self.debug = debug
     Patient.patients_num += 1
     self.patients_num = Patient.patients_num
-    if self.debug: print(f"{self} arrives at {env.now:.2f}")
+    logger.log(f"{self} arrives at {env.now:.2f}", 5)
     self.arrival_time = env.now
 
   def arrival(self, facilities, dataset):
@@ -104,8 +116,11 @@ class Patient:
       yield self.env.process(facilities.preparation(self))
 
     with facilities.operation_facilities.request() as request1, facilities.recovery_facilities.request() as request2:
-      yield request1 & request2
+      yield request1
       yield self.env.process(facilities.operation(self))
+
+    with facilities.recovery_facilities.request() as request2:
+      yield request2
       yield self.env.process(facilities.recovery(self))
       Patient.patients_treated += 1
       dataset.add_patient(self)
@@ -126,6 +141,7 @@ class Patient:
 
   def get_data_headers():
     return ["arrival_queue_time", "preparation_time", "operation_queue_time", "operation_time", "recovery_time", "total_time", "complication_occurred"]
+
 
 class Dataset:
   """Stores and analyzes the data of all patients that arrive at the hospital. When patient has been treated
@@ -148,42 +164,36 @@ class Dataset:
     avg_arrival_queue_time = round(np.average(dataset[:,0]),2)
     operation_utilization_time = round(np.sum(dataset[:,3]),2)
     operation_utilization_percentage = round((operation_utilization_time / SIM_TIME)*100, 2)
-    print("Average arrival queue time:", avg_arrival_queue_time)
-    print(f"Operation room utilization time: {operation_utilization_time} ({operation_utilization_percentage} %)")
-    print(f"Complications during operations: {Patient.complications}")
+    logger.log(f"Average arrival queue time: {avg_arrival_queue_time}", 1)
+    logger.log(f"Operation room utilization time: {operation_utilization_time} ({operation_utilization_percentage} %)", 1)
+    logger.log(f"Complications during operations: {Patient.complications}", 1)
   def __str__(self):
     return f"{self.get_dataset()}"
 
+
 dataset = Dataset()
-def hospital(env, debug):
+def hospital(env):
   """The main process of the simulation. Starts the simulation."""
-  facilities = FacilitiesManager(env, debug)
+  facilities = FacilitiesManager(env)
 
   while True:
-    yield env.timeout(np.random.randint(PATIENT_INTERARRIVAL_TIME - 1, PATIENT_INTERARRIVAL_TIME + 1))
-    patient = Patient(env, debug)
+    interarrival = random.expovariate(PATIENT_INTERARRIVAL_TIME)
+    yield env.timeout(interarrival)
+    patient = Patient(env)
     env.process(patient.arrival(facilities, dataset))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--preparation', metavar='p', required=False, dest='preparation', type=int, help='Number of preparation facilities')
-parser.add_argument('--recovery', metavar='r', required=False, dest='recovery', type=int, help="Number of recovery facilities")
-args = parser.parse_args()
 
-if args.recovery != None:
-  NUM_RECOVERY_FACILITIES = args.recovery
-if args.preparation != None: 
-  NUM_PREPARATION_FACILITIES = args.preparation
+def main():
+  logger.log("Starting simulation!", 1)
+  env.process(hospital(env))
+  env.run(until=SIM_TIME)
+  logger.log("Simulation ended!", 1) 
+  logger.log(f"Patients arrived: {Patient.patients_num}", 1)
+  logger.log(f"Patients treated: {Patient.patients_treated}", 1)
+  print(Patient.get_data_headers())
+  print(dataset)
+  dataset.analyze()
 
-print("Number of recovery facilities:", NUM_RECOVERY_FACILITIES)
-print("Number of preparation facilities:", NUM_PREPARATION_FACILITIES)
-
-print("Starting simulation!")
-env.process(hospital(env, DEBUG))
-env.run(until=SIM_TIME)
-print("Simulation ended!") 
-print("Patients arrived: ", Patient.patients_num)
-print("Patients treated: ", Patient.patients_treated)
-print(Patient.get_data_headers())
-print(dataset)
-dataset.analyze()
+if __name__ == "__main__":
+    main()
 
