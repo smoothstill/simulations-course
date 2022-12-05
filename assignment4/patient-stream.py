526fc372f16351d02af8cdfd470b7db79aa3e404
Mika Lammi <59178987+smoothstill@users.noreply.github.com>
2022-12-05 23:07:00 +0200

    fixes to assignment2, added assignment4

--- assignment4/patient-stream.py
+++ assignment4/patient-stream.py
@@ -3,6 +3,7 @@ import simpy # version 4.0.1
 # Python version 3.10.6
 import random
 from scipy.stats import t
+import math
 
 # Constants of the simulation. Assuming that time is in minutes
 N_PREPARATION_FACILITIES = 3
@@ -43,26 +44,6 @@ class FacilitiesManager:
   env = None
   logger = None
   include_twist = None
-
-  prep_idle_start_time = None
-  prep_idle_end_time = None
-  op_idle_start_time = None
-  op_idle_end_time = None
-  rec_idle_start_time = None
-  rec_idle_end_time = None
-  
-  prep_idle_time = None
-  op_idle_time = None
-  rec_idle_time = None
-
-  def stop_prep_idle(self):
-    if self.prep_idle_start_time != None:
-      self.prep_idle_end_time = self.env.now
-      idle_time = round(self.prep_idle_end_time - self.prep_idle_start_time, 2)
-      self.prep_idle_time += idle_time
-      self.prep_idle_start_time = None
-    else:
-      raise Exception("Preparation facilities were not idling")
     
   def __init__(
       self, 
@@ -75,9 +56,13 @@ class FacilitiesManager:
       op_time = OPERATION_TIME, 
       rec_time = RECOVERY_TIME, 
       compl_chance = COMPLICATION_CHANGE, 
-      include_twist = True
+      include_twist = True,
+      prep_time_unif = None,
+      op_time_unif = None,
+      rec_time_unif = None,
     ):
     self.logger = Logger(verbose=verbose)
+
     self.env = env
     self.preparation_facilities = simpy.Resource(env, p)
     self.operation_facilities = simpy.Resource(env, o)
@@ -85,32 +70,31 @@ class FacilitiesManager:
     self.prep_time = prep_time
     self.op_time = op_time
     self.rec_time = rec_time
+    
     self.compl_ratio = compl_chance
     self.include_twist = include_twist
 
-    self.prep_idle_start_time = self.env.now
-    self.prep_idle_end_time = None
-    self.op_idle_start_time = None
-    self.op_idle_end_time = None
-    self.rec_idle_start_time = None
-    self.rec_idle_end_time = None
+    self.prep_time_unif = prep_time_unif
+    self.op_time_unif = op_time_unif
+    self.rec_time_unif = rec_time_unif
 
   def preparation(self, patient):
     self.logger.log(f"{patient} enters preparation facility at {self.env.now:.2f}", 5)    
-    random_time = random.expovariate(1/self.prep_time)
+    random_time = random.expovariate(1/self.prep_time) if self.prep_time_unif == None else random.uniform(self.prep_time_unif[0], self.prep_time_unif[1])
     self.logger.log(f"{patient} preparation time is {random_time}", 5)
     yield self.env.timeout(random_time)
     self.logger.log(f"  {patient} leaves preparation facility at {self.env.now:.2f}", 5)
 
   def operation(self, patient):
     self.logger.log(f"{patient} enters operation facility at {self.env.now:.2f}", 5)
-    random_time = random.expovariate(1/self.op_time)
+    random_time = random.expovariate(1/self.op_time) if self.op_time_unif == None else random.uniform(self.op_time_unif[0], self.op_time_unif[1])
     self.logger.log(f"{patient} operation time is {random_time}")
     yield self.env.timeout(random_time)
-    if (self.include_twist and random.uniform(0, 1) <= self.compl_ratio):
+    if (self.include_twist and np.random.uniform(0, 1) <= self.compl_ratio):
       self.logger.log(f"Complication occurred for {patient}, patient is put to high priority recovery queue", 5)
       patient.treatment_priority = 1
       patient.dataset.patient_complications += 1
+      patient.complication_occurred = True
     self.logger.log(f"  {patient} leaves operation facility at {self.env.now:.2f}", 5)
 
   def recovery(self, patient):
@@ -118,7 +102,7 @@ class FacilitiesManager:
     recovery_time_multiplier = 1
     if patient.treatment_priority == 1:
       recovery_time_multiplier = 2
-    random_time = random.expovariate(1/self.rec_time*recovery_time_multiplier)
+    random_time = random.expovariate(1/self.rec_time*recovery_time_multiplier) if self.rec_time_unif == None else random.uniform(self.rec_time_unif[0], self.rec_time_unif[1])
     self.logger.log(f"{patient} recovery time is {random_time}")
     yield self.env.timeout(random_time)
     self.logger.log(f"  {patient} leaves recovery facility at {self.env.now:.2f}", 5)
@@ -128,16 +112,29 @@ class Patient:
   all arrived patients and treated patients"""
   patients_num = 0
   treatment_priority = 10 # Default priority
-  arrival_time = None
   dataset = None
   facilities = None
   env = None
   logger = None
 
+  arrival_time = None
+  operation_blocked_time = None
+  preparation_queue_length = None
+  operation_time = None
+  total_time = None
+  complication_occurred = False
+
   def __init__(self, env, facilities, dataset, verbose=VERBOSE):
     self.treatment_priority = 10
     self.patients_num = 0
+
     self.arrival_time = None
+    self.operation_blocked_time = None
+    self.preparation_queue_length = None
+    self.operation_time = None
+    self.total_time = None
+    self.complication_occurred = False
+
     self.dataset = dataset
     self.facilities = facilities
     self.logger = Logger(verbose)
@@ -150,6 +147,7 @@ class Patient:
   def arrival(self):
     prep_q_len = len(self.facilities.preparation_facilities.queue)
     self.dataset.preparation_queue_lengths.append(prep_q_len)
+    self.preparation_queue_length = prep_q_len
     with self.facilities.preparation_facilities.request() as in_prep_room:
       yield in_prep_room
       yield self.env.process(self.facilities.preparation(self))
@@ -159,32 +157,36 @@ class Patient:
     self.operation_queue_length = op_q
     with self.facilities.operation_facilities.request() as in_op_room, self.facilities.recovery_facilities.request(priority=self.treatment_priority) as in_rec_room:
       yield in_op_room
-      operation_time_start = self.env.now
+      operation_time_start = float(self.env.now)
       yield self.env.process(self.facilities.operation(self))
-      block_start_time = self.env.now
+      block_start_time = float(self.env.now)
       rec_q = len(self.facilities.recovery_facilities.queue)
       self.recovery_queue_length = rec_q
       yield in_rec_room
-      block_end_time = self.env.now
+      block_end_time = float(self.env.now)
       self.facilities.operation_facilities.release(in_op_room)
-      block_time = round(block_end_time - block_start_time, 2)
-      operation_time = round(block_end_time - operation_time_start, 2)
+      block_time = block_end_time - block_start_time
+      operation_time = block_end_time - operation_time_start
+      self.operation_blocked_time = block_time
+      self.operation_time = operation_time
       self.dataset.operation_blocked_time += block_time
       self.dataset.operation_time += operation_time
       yield self.env.process(self.facilities.recovery(self))
       self.facilities.recovery_facilities.release(in_rec_room)
       self.dataset.patients_treated += 1
+      self.total_time = float(float(self.env.now) - self.arrival_time)
+      self.dataset.add_patient(self)
 
   def __str__(self):
     return f"Patient no. {self.patients_num}"
 
   def get_data(self):
     complication_occurred = float(self.complication_occurred)
-    data = np.array([self.arrival_queue_time, self.preparation_time, self.operation_queue_time, self.operation_time, self.recovery_time, self.total_time, complication_occurred, self.preparation_queue_length, self.operation_queue_length, self.recovery_queue_length])
+    data = np.array([self.arrival_time,  self.preparation_queue_length, self.operation_blocked_time, self.operation_time, self.operation_time, complication_occurred, self.total_time])
     return data
 
   def get_data_headers():
-    return ["arrival_queue_time", "preparation_time", "operation_queue_time", "operation_time", "recovery_time", "total_time", "complication_occurred", "preparation_queue_length", "operation_queue_length", "recovery_queue_length"]
+    return ["arrival_time",  "preparation_queue_length", "operation_blocked_time", "operation_time", "operation_time", "complication_occurred", "total_time"]
 
 
 class Dataset:
@@ -226,24 +228,36 @@ class Dataset:
     dataset = np.array(temp_list)
     return dataset
 
+  def split_dataset(self, splits):
+    dataset = self.get_dataset()
+    chunks = (splits*2)+1
+    num_elements = math.floor(len(dataset)/chunks)
+    splits = []
+    for i in range(0, chunks):
+      split = dataset[i*num_elements:min(i*num_elements+num_elements, len(dataset))]
+      splits.append(split)
+
+    splitted = splits[1::2]
+    return splitted
+
   def analyze(self):
     op_block_ratio = round(self.operation_blocked_time / self.operation_time, 2)
     avg_prep_q_len = sum(self.preparation_queue_lengths)/len(self.preparation_queue_lengths)
     complications = self.patient_complications
-    self.logger.log(f"Operation was blocked for {op_block_ratio} of the time", 4)
-    self.logger.log(f"Average preparation queue length: {avg_prep_q_len}", 4)
-    self.logger.log(f"Complications during operations: {complications}", 4)
+    self.logger.log(f"Operation was blocked for {op_block_ratio*100}% of the time", 1)
+    self.logger.log(f"Average preparation/entrance queue length: {avg_prep_q_len}", 1)
+    self.logger.log(f"Complications during operations: {complications}", 1)
     return op_block_ratio, avg_prep_q_len, complications
   
   def __str__(self):
     return f"{self.get_dataset()}"
 
 
-def hospital(env, dataset, facilities):
+def hospital(env, dataset, facilities, interarrival_time=PATIENT_INTERARRIVAL_TIME, interarrival_time_unif=None):
   """The main simulation cycle."""
 
   while True:
-    interarrival = random.expovariate(1/PATIENT_INTERARRIVAL_TIME)
+    interarrival = random.expovariate(1/interarrival_time) if interarrival_time_unif == None else random.uniform(interarrival_time_unif[0], interarrival_time_unif[1])
     yield env.timeout(interarrival)
     patient = Patient(env, facilities=facilities, dataset=dataset, verbose=VERBOSE)
     env.process(patient.arrival())
@@ -255,58 +269,55 @@ def confidence_interval(m, s, t_crit, len_x):
 
 def main():
 
+  np.set_printoptions(suppress=True)
   logger = Logger(verbose=VERBOSE)
   logger.log("====================================================================", 1)
-  setups = [
-    (3, 4, True),
-    (3, 5, True),
-    (4, 5, True),
-    (3, 4, False),
-    (3, 5, False),
-    (4, 5, False)
+
+  configurations = [
+    dict(p=2, r=4, prep_time=40, prep_time_unif=None, rec_time=40, rec_time_unif=None, inter_time=25, inter_time_unif=None, compl_chance=0.05),
+    dict(p=2, r=4, prep_time=None, prep_time_unif=(30,50), rec_time=None, rec_time_unif=(30,50), inter_time=25, inter_time_unif=None, compl_chance=0.05),
+    dict(p=2, r=4, prep_time=40, prep_time_unif=None, rec_time=40, rec_time_unif=None, inter_time=None, inter_time_unif=(20,30), compl_chance=0.05),
+    dict(p=2, r=4, prep_time=None, prep_time_unif=(30,50), rec_time=None, rec_time_unif=(30,50), inter_time=None, inter_time_unif=(20,30), compl_chance=0.05),
+    dict(p=2, r=4, prep_time=40, prep_time_unif=None, rec_time=40, rec_time_unif=None, inter_time=22.5, inter_time_unif=None, compl_chance=0.05),
+    dict(p=2, r=4, prep_time=None, prep_time_unif=(30,50), rec_time=None, rec_time_unif=(30,50), inter_time=22.5, inter_time_unif=None, compl_chance=0.05),
+    dict(p=2, r=4, prep_time=40, prep_time_unif=None, rec_time=40, rec_time_unif=None, inter_time=None, inter_time_unif=(20,25), compl_chance=0.05),
+    dict(p=2, r=4, prep_time=None, prep_time_unif=(30,50), rec_time=None, rec_time_unif=(30,50), inter_time=None, inter_time_unif=(20,25), compl_chance=0.05),
   ]
 
-  for (p, r, c) in setups:
-    logger.log(f"Simulation with (p={p}, r={r}), random seed = {RANDOM_SEED}, include twist: {c}", 1)
-    data = []
+  for conf in configurations:
+    sim_time = 10000
     np.random.seed(RANDOM_SEED)
     random.seed(RANDOM_SEED)
-    for i in range(20):
-      env = simpy.Environment()
-      dataset = Dataset(verbose=VERBOSE)
-      facilities = FacilitiesManager(env,verbose=VERBOSE, p=p, r=r, include_twist=c)
-      logger.log( f"Simulation no. {i}", 4)
-      env.process(hospital(env, dataset=dataset, facilities=facilities))
-      env.run(until=SIM_TIME)
-      logger.log(f"Patients arrived: {dataset.patients_arrived}", 4)
-      logger.log(f"Patients treated: {dataset.patients_treated}", 4)
-      op_blocked_ratio, avg_prep_q_len, n_complications = dataset.analyze()
-      data.append([op_blocked_ratio, avg_prep_q_len, n_complications])
-    data_array = np.array(data)
+    env = simpy.Environment()
+    logger.log(f"Simulation with (p={conf['p']}, r={conf['r']}), random seed = {RANDOM_SEED}, include twist: {True}, prep_time: {conf['prep_time'] if conf['prep_time_unif'] == None else conf['prep_time_unif']}, rec_time: {conf['rec_time'] if conf['rec_time_unif'] == None else conf['rec_time_unif']}, inter_time: {conf['inter_time'] if conf['inter_time_unif'] == None else conf['inter_time_unif']}, compl_chance:{conf['compl_chance']}", 1)
+    dataset = Dataset(verbose=VERBOSE)
+    facilities = FacilitiesManager(
+      env,verbose=VERBOSE, 
+      p=conf['p'], 
+      r=conf['r'], 
+      include_twist=True, 
+      prep_time=conf['prep_time'], 
+      rec_time=conf['rec_time'], 
+      prep_time_unif=conf['prep_time_unif'], 
+      rec_time_unif=conf['rec_time_unif'],
+      compl_chance=conf['compl_chance']
+      )
+    env.process(hospital(env, dataset=dataset, facilities=facilities, interarrival_time=conf['inter_time'], interarrival_time_unif=conf['inter_time_unif']))
+    env.run(until=sim_time)
 
-    confidence = 0.95
-    dof = len(data_array[:,0]) - 1
-    len_data = len(data_array[:,0])
-    t_crit = np.abs(t.ppf((1-confidence)/2,dof))
+    samples = dataset.split_dataset(10)
+    correlations = []
 
-    # blocked ratio 
-    b_mean = data_array[:,0].mean()
-    b_std = data_array[:,0].std()
-    b_interval = confidence_interval(m=b_mean, s=b_std, t_crit=t_crit, len_x=len_data)
+    for i in range(1, len(samples)):
+      correlation = np.corrcoef(samples[i-1][:,1], samples[i][:,1])
+      if math.isnan(correlation[1][0]):
+        correlations.append(0)
+      else:
+        correlations.append(correlation[1][0])
 
-    # preparation queue length
-    p_mean = data_array[:,1].mean()
-    p_std = data_array[:,1].std()
-    p_interval = confidence_interval(m=p_mean, s=p_std, t_crit=t_crit, len_x=len_data)
+    logger.log(f"correlations between adjacent samples:\n{correlations}", 1)
+    dataset.analyze()
 
-    # complications
-    c_mean = data_array[:,2].mean()
-    c_std = data_array[:,2].std()
-    c_interval = confidence_interval(m=c_mean, s=c_std, t_crit=t_crit, len_x=len_data)
-
-    logger.log(f"Operation blocked ratio mean: {b_mean:.2f}, 95% confidence interval: {b_interval}", 1)
-    logger.log(f"Preparation q length mean: {p_mean:.2f}, 95% confidence interval: {p_interval}", 1)
-    logger.log(f"Complications mean: {c_mean:.2f}, 95% confidence interval: {c_interval}", 1)
     logger.log("====================================================================", 1)
 
 if __name__ == "__main__":
